78ed03f7133d17b05bd98eca676a839d8295bca6
Neha V. K <61344276+Neha1594@users.noreply.github.com>
2020-06-12 17:18:23 +0530

    passing data usind codable and plist

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,19 +10,37 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["Udemy course", "web series", "Buy Cosmatics"]
-    let defaults = UserDefaults.standard
+    var itemArray = [Item]()
+    
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask) .first?.appendingPathComponent("Items.plist")
+    
+    
     
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        print(dataFilePath)
         
+        
+        let newItem = Item()
+        newItem.title = "Udemy course"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Max shopping"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Nykyaa delivery"
+        itemArray.append(newItem3)
         //to save current added item in app list
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
+//        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
+      //     itemArray = items
+    //    }
 
         
         // Do any additional setup after loading the view, typically from a nib.
@@ -43,21 +61,27 @@ class TodoListViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        // Ternary operator ==>
+        //value =  condition ? valueIfTrue : valueIfFalse
+        
+        cell.accessoryType = item.done == true ? .checkmark : .none
+        
+        
         return cell
     }
     
     //Tableview delegate method
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-       
-       // code for select and deselct row and add checkmaek to it
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+       saveIrems()
+    
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -70,13 +94,15 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add new item to Todoey", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //What will happen once the clicks add item
-            self.itemArray.append(textField.text!)
             
-           self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
-            //add and display added item to table view reload() method used
+            let newItem = Item()
+            newItem.title = textField.text!
             
-            self.tableView.reloadData() 
+            self.itemArray.append(newItem)
+            self.saveIrems()
+            
+          
         }
         
         alert.addTextField { (alertTextFeild) in
@@ -84,17 +110,43 @@ class TodoListViewController: UITableViewController {
             print(alertTextFeild.text)
             textField = alertTextFeild
             
-            print(alertTextFeild.text)
+            
         }
             alert.addAction(action)
             
             present(alert, animated: true, completion: nil)
         }
+    
+    //Model Manupulation Methods
+    
+    func saveIrems(){
+        let encoder = PropertyListEncoder()
+        do{
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        }catch{
+            print("Error encoding item array, \(error)")
+        }
         
+        
+        //add and display added item to table view reload() method used
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadItems(){
+        if let data = try? Data(contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            do{
+            itemArray = try decoder.decode([Item].self, from: data)
+            }catch{
+                print("Error is print, \(error)")
+            }
+    }
         
         
     }
-    
+}
     
 
 
